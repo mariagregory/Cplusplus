26f4a0fa76a876d65de4c9c004392210c1d81d38
Maria Gregory <35348733+uralmasha@users.noreply.github.com>
2018-11-27 02:34:39 -0800

    Delete all_1.js

--- MIDTERM Project - Battleship/HTML by Doxygen/html/search/all_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['c_5fstandard_5fheaders_5findexer_2ec',['c_standard_headers_indexer.c',['../c__standard__headers__indexer_8c.html',1,'']]],
-  ['collisn',['collisn',['../main_8cpp.html#ae5435781037160a2581f92ef1a756592',1,'main.cpp']]],
-  ['cpp_5fstandard_5fheaders_5findexer_2ecpp',['cpp_standard_headers_indexer.cpp',['../cpp__standard__headers__indexer_8cpp.html',1,'']]]
-];

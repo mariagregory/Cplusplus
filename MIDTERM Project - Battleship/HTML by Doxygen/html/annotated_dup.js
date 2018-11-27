98bad0f2e2b526519bf6d6ed7f67337e8f4ac9d3
Maria Gregory <35348733+uralmasha@users.noreply.github.com>
2018-11-27 02:36:34 -0800

    Delete annotated_dup.js

--- MIDTERM Project - Battleship/HTML by Doxygen/html/annotated_dup.js
+++
@@ -1,6 +0,0 @@
-var annotated_dup =
-[
-    [ "Board", "struct_board.html", "struct_board" ],
-    [ "Player", "struct_player.html", "struct_player" ],
-    [ "Ship", "struct_ship.html", "struct_ship" ]
-];
